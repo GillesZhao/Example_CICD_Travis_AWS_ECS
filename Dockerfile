af075c353273444bb90c9b8078fc51e2d2f41a7b
root <1815247@qq.com>
2020-01-07 22:24:16 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
-FROM python:3.7-alpine
+FROM python:3.7-alpine AS app-python
 ADD . /code
 WORKDIR /code
 RUN pip install -r requirements.txt
 CMD ["python", "app.py"]
+
+FROM redis:alpine AS app-redis
--- app.py
+++ app.py
@@ -1,12 +1,21 @@
+#!/usr/bin/python2.7
 import time  
 import redis
 from flask import Flask
 
 app = Flask(__name__)
-#cache = redis.Redis(host='redis', port=6379)
+cache = redis.Redis(host='redis', port=6379)
+
 def get_hit_count():
-    count = 10
-    return count
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 @app.route('/')
 def hello():
@@ -18,3 +27,4 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
+
