1273f437f00bb74e8a557f32c6d1ae9c56e14159
Gary Zhao <1815247@qq.com>
2020-01-21 17:30:18 +0800

    ok

--- create_alb_listener_rules.sh
+++ create_alb_listener_rules.sh
@@ -3,4 +3,4 @@ aws elbv2 create-rule \
     --listener-arn arn:aws:elasticloadbalancing:ap-southeast-1:468969217647:listener/app/alb-ecs-poc/4dc026513826bb09/5e24430998b64e52 \
     --priority 10 \
     --conditions '{ "Field": "host-header", "HostHeaderConfig": { "Values": ["'"$TRAVIS_BRANCH"'.*"]  }  }' \
-    --actions Type=forward,TargetGroupArn=arn:aws:elasticloadbalancing:ap-southeast-1:468969217647:targetgroup/hw-2877/7a77be6b294ef1b7
+    --actions Type=forward,TargetGroupArn=arn:aws:elasticloadbalancing:ap-southeast-1:468969217647:targetgroup/hw-2877/e436d956a03590d8
