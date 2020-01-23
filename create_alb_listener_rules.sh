e9f920b2f3b341773ef8efb4bbf2f6df755a4f0f
Gary Zhao <1815247@qq.com>
2020-01-23 11:01:01 +0800

    bug fixed

--- create_alb_listener_rules.sh
+++ create_alb_listener_rules.sh
@@ -4,10 +4,17 @@ targetgrouparn=`aws elbv2 describe-target-groups | grep -w $TRAVIS_BRANCH | grep
 targetgrouparn=`echo ${targetgrouparn##*: \"}`
 targetgrouparn=`echo ${targetgrouparn%%\"*}`
 
+aws elbv2 describe-rules --listener-arn arn:aws:elasticloadbalancing:ap-southeast-1:468969217647:listener/app/alb-ecs-poc/4dc026513826bb09/5e24430998b64e52 | grep -w $TRAVIS_BRANCH 
+
+if [ $? -ne 0 ];then
+ 
 aws elbv2 create-rule \
     --listener-arn arn:aws:elasticloadbalancing:ap-southeast-1:468969217647:listener/app/alb-ecs-poc/4dc026513826bb09/5e24430998b64e52 \
     --priority $RANDOM \
     --conditions '{ "Field": "host-header", "HostHeaderConfig": { "Values": ["'"$TRAVIS_BRANCH"'.*"]  }  }' \
     --actions Type=forward,TargetGroupArn=$targetgrouparn
-
-exit 0    
+else
+  echo "listener rule already exists"
+  exit 0    
+  
+fi
