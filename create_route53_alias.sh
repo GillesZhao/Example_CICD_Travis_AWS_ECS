d2e15b92358ecbf98bb64482cef729dcb176c147
Gary Zhao <1815247@qq.com>
2020-01-31 23:24:54 +0800

    ok

--- create_route53_alias.sh
+++ create_route53_alias.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+if [ $deletion_mark -ne 1 ];then
+
 aws route53 change-resource-record-sets --hosted-zone-id Z14JIGC687R7OP --change-batch '{ "Comment": "Route53 creating a record set", "Changes": [ { "Action": "CREATE", "ResourceRecordSet": { "Name": "'"$TRAVIS_BRANCH"'.juwai.xyz.", "Type": "A", "AliasTarget":{ "HostedZoneId": "Z1LMS91P8CMLE5", "DNSName": "dualstack.alb-ecs-poc-1103874013.ap-southeast-1.elb.amazonaws.com","EvaluateTargetHealth": false} } } ] }'
 
 exit 0
+
+else
+  aws route53 change-resource-record-sets --hosted-zone-id Z14JIGC687R7OP --change-batch '{ "Comment": "Route53 creating a record set", "Changes": [ { "Action": "DELETE", "ResourceRecordSet": { "Name": "'"$TRAVIS_BRANCH"'.juwai.xyz.", "Type": "A", "AliasTarget":{ "HostedZoneId": "Z1LMS91P8CMLE5", "DNSName": "dualstack.alb-ecs-poc-1103874013.ap-southeast-1.elb.amazonaws.com","EvaluateTargetHealth": false} } } ] }'
+
+fi
