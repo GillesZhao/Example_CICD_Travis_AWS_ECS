6c76c03d732f5cea2cec566797cdcdc421620ff9
Gary Zhao <1815247@qq.com>
2020-02-05 15:53:04 +0800

    ok

--- destroy_resources.sh
+++ destroy_resources.sh
@@ -9,15 +9,23 @@ rule_arn=`aws elbv2 describe-rules --listener-arn arn:aws:elasticloadbalancing:a
 
 if [ $deletion_mark -eq 1 ];then
 
-#Delete ALB listener rules
+#Delete ALB listener rule
    if [ -n "$rule_arn" ];then
     aws elbv2 delete-rule \
      --rule-arn $rule_arn
+    echo "ALB listener rule deleted" 
+   else
+    echo "ALB listener rule doesn't exist"
    fi
 
 #Delete ALB target group 
-  aws elbv2 delete-target-group \
+   if [ -n "$targetgrouparn" ];then
+    aws elbv2 delete-target-group \
     --target-group-arn $targetgrouparn  
+    echo "ALB target group deleted" 
+   else
+    echo "ALB target group doesn't exist"
+   fi
 
 #Delete ECS service 
   aws ecs delete-service \
@@ -25,8 +33,15 @@ if [ $deletion_mark -eq 1 ];then
   --service $TRAVIS_BRANCH \
   --force 
 
+  echo "ECS service deleted"
+
 #Delete Route53 record set 
   aws route53 change-resource-record-sets --hosted-zone-id Z14JIGC687R7OP   --change-batch '{ "Comment": "Route53 creating a record set", "Changes": [ { "Action": "DELETE", "ResourceRecordSet": { "Name": "'"$TRAVIS_BRANCH"'.juwai.xyz.", "Type": "A", "AliasTarget":{ "HostedZoneId": "Z1LMS91P8CMLE5", "DNSName": "dualstack.alb-ecs-poc-1103874013.ap-southeast-1.elb.amazonaws.com","EvaluateTargetHealth": false} } } ] }'
 
+  if [ $? == 0 ];then
+    echo "Route53 record set deleted"
+  else
+    echo "Route53 record set doesn't exist or already deleted"
+  fi
 fi
 
