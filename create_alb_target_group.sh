5c19b566535034cbb48b98326ef0169b3fd41b61
Gary Zhao <1815247@qq.com>
2020-02-05 14:47:33 +0800

    ok

--- create_alb_target_group.sh
+++ create_alb_target_group.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+if [ $deletion_mark -ne 1 ];then
+
 aws elbv2 create-target-group \
     --name $TRAVIS_BRANCH \
     --protocol HTTP \
     --port 80 \
     --target-type ip \
     --vpc-id vpc-00016262d7b12e54c
+
+fi
