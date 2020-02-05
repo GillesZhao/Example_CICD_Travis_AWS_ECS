9e528fb9e2fdf6dd5e1389488d0f6e1ca4c6c276
Gary Zhao <1815247@qq.com>
2020-02-05 17:02:28 +0800

    ok

--- create_alb_listener_rules.sh
+++ create_alb_listener_rules.sh
@@ -13,14 +13,13 @@ aws elbv2 describe-rules --listener-arn arn:aws:elasticloadbalancing:ap-southeas
   if [ $? -ne 0 ];then
  
     aws elbv2 create-rule \
-    --listener-arn arn:aws:elasticloadbalancing:ap-southeast-1:468969217647:listener/    app/alb-ecs-poc/4dc026513826bb09/5e24430998b64e52 \
+    --listener-arn arn:aws:elasticloadbalancing:ap-southeast-1:468969217647:listener/app/alb-ecs-poc/4dc026513826bb09/5e24430998b64e52 \
     --priority $RANDOM \
     --conditions '{ "Field": "host-header", "HostHeaderConfig": { "Values":["'"$TRAVIS_BRANCH"'.*"]  }  }' \
     --actions Type=forward,TargetGroupArn=$targetgrouparn
   else
     echo "listener rule already exists"
-  exit 0    
-  
+    exit 0    
   fi
 
 else
