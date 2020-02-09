05ed812b744c5053f7022bb4b320df983e0044c2
Gary Zhao <1815247@qq.com>
2020-02-09 15:42:34 +0800

    ok

--- create_alb_listener_rules.sh
+++ create_alb_listener_rules.sh
@@ -4,11 +4,11 @@ targetgrouparn=`aws elbv2 describe-target-groups | grep -w $TRAVIS_BRANCH | grep
 targetgrouparn=`echo ${targetgrouparn##*: \"}`
 targetgrouparn=`echo ${targetgrouparn%%\"*}`
 
-rule_arn=`aws elbv2 describe-rules --listener-arn arn:aws:elasticloadbalancing:ap-southeast-1:468969217647:listener/app/alb-ecs-poc/4dc026513826bb09/5e24430998b64e52 | jq -c '.Rules[]| select(.Conditions[].Values[]| contains("'"$TRAVIS_BRANCH"'"))' | jq -r '.RuleArn'`
+rule_host_head_value=`aws elbv2 describe-rules --listener-arn arn:aws:elasticloadbalancing:ap-southeast-1:468969217647:listener/app/alb-ecs-poc/4dc026513826bb09/5e24430998b64e52 | jq -c '.Rules[]| select(.Conditions[].Values[]| contains("'"$TRAVIS_BRANCH"'"))' | jq -r .Conditions[].Values[] | grep -w $TRAVIS_BRANCH`
 
 if [ $deletion_mark -ne 1 ];then
 
-  if [ -z "$rule_arn" ];then
+  if [ -z "$rule_host_head_value" ];then
     aws elbv2 create-rule \
     --listener-arn arn:aws:elasticloadbalancing:ap-southeast-1:468969217647:listener/app/alb-ecs-poc/4dc026513826bb09/5e24430998b64e52 \
     --priority $RANDOM \
