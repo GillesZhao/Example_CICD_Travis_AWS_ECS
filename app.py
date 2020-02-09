65e39fe3ea86f207b3c89eb660ee97fe93f950c4
Gary Zhao <1815247@qq.com>
2020-02-09 21:29:20 +0800

    ok

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def hello():
       #return 'This is Juwai old website. count = {}\n'.format(count)
       #return 'HW-277 RPC IQI agent registion . count = {}\n'.format(count)
       #return 'HW-287 RPC function for password . \n'.format(count)
-      return '{} master RPC python version upgrade.\n'.format(random.randint(50000,90000)) 
+      return '{} master branch RPC python version upgrade.\n'.format(random.randint(50000,90000)) 
     elif count < 5000:
       return 'Hello Guys! Juwai website version {} is coming.\n'.format(count)
     else:
