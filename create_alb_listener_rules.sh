312993ec3f1498ea8e2f38a46966f9b3c7660c2a
Gary Zhao <1815247@qq.com>
2020-02-09 15:20:02 +0800

    ok

--- create_alb_listener_rules.sh
+++ create_alb_listener_rules.sh
@@ -8,10 +8,7 @@ rule_arn=`aws elbv2 describe-rules --listener-arn arn:aws:elasticloadbalancing:a
 
 if [ $deletion_mark -ne 1 ];then
 
-aws elbv2 describe-rules --listener-arn arn:aws:elasticloadbalancing:ap-southeast-1:468969217647:listener/app/alb-ecs-poc/4dc026513826bb09/5e24430998b64e52 | grep -w $TRAVIS_BRANCH 
-
-  if [ $? -ne 0 ];then
- 
+  if [ -z "$rule_arn" ];then
     aws elbv2 create-rule \
     --listener-arn arn:aws:elasticloadbalancing:ap-southeast-1:468969217647:listener/app/alb-ecs-poc/4dc026513826bb09/5e24430998b64e52 \
     --priority $RANDOM \
@@ -20,10 +17,8 @@ aws elbv2 describe-rules --listener-arn arn:aws:elasticloadbalancing:ap-southeas
     echo -e "\033[34m listener rule created \033[0m"
   else
     echo -e "\033[31m listener rule already exists \033[0m"
-    exit 0    
   fi
 
 else
   echo -e "\033[31m This is a resources deletion operation. \033[0m"
-
 fi     
